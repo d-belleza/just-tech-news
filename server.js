c4301666d735026dc8924eaf8f7acf060cf74055
davidbelleza <davidbelleza@gmail.com>
2020-08-20 15:36:43 -0500

    refactor code

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,7 +27,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess))
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
